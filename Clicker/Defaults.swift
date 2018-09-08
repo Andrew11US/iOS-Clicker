ebe85e963fcef2a8eb40d65cdeb1a3d1ac11626a
Andrii Halabuda <losAngelesBoy11@gmail.com>
2018-09-08 17:53:20 +0200

    iPhone UI

---
+++ Clicker/Defaults.swift
@@ -0,0 +1,12 @@
+//
+//  Defaults.swift
+//  Clicker
+//
+//  Created by Andrew on 9/8/18.
+//  Copyright © 2018 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+let defaults = UserDefaults.standard
